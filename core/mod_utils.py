808969bddab3090bf12c22a9996e616980fa3ce9
ShawK91 <khadkas@oregonstate.edu>
2018-10-25 21:07:11 -0700

    argparse type fixes

--- core/mod_utils.py
+++ core/mod_utils.py
@@ -1,6 +1,6 @@
 from torch import nn
 from torch.autograd import Variable
-import random, pickle, copy
+import random, pickle, copy, argparse
 import numpy as np, torch, os
 
 class Tracker(): #Tracker
@@ -58,6 +58,13 @@ class Tracker(): #Tracker
                 np.savetxt(filename, np.array(var[2]), fmt='%.3f', delimiter=',')
 
 
+def str2bool(v):
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
 
 
 def to_numpy(var):
