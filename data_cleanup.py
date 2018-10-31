84df45aa55d373cec27a71127dfe88e88b71caaa
ShawK91 <khadkas@oregonstate.edu>
2018-10-30 20:47:28 -0700

    Data cleanup finds and removes corrupted npz files

---
+++ data_cleanup.py
@@ -0,0 +1,33 @@
+import numpy as np, os
+import argparse
+
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-data_folder', help='Data folder',  required=True)
+data_folder = vars(parser.parse_args())['data_folder']
+
+
+
+######## READ DATA #########
+list_files = os.listdir(data_folder)
+
+corrupt_list = []
+for index, file in enumerate(list_files):
+        print('Checking file', file)
+        try:
+            data = np.load(data_folder + file)
+            s = data['state'];
+            ns = data['next_state'];
+            a = data['action'];
+            r = data['reward'];
+            done_dist = data['done_dist']
+        except:
+            corrupt_list.append(file)
+            os.remove(file)
+
+print('Corrupt files removed', corrupt_list)
+
+
+
+
